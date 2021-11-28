1d027c80728eec542c682602512d6a1153edc333
Corentyn_ <40527889+Corentynn@users.noreply.github.com>
2021-11-28 12:24:06 +0100

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,17 +1,13 @@
 <?php
     session_start();
     define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'register_user_system');
-    define('DB_PASSWORD', 'J4GbnHz7lOKLAMJg');
+    define('DB_USERNAME', 'username');
+    define('DB_PASSWORD', 'password');
     define('DB_NAME', 'register_user_system');
 
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     
     // Check connection
     if($link === false){
-        echo "<script>console.log('fuck');</script>";
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }else{
-        echo "<script>console.log('it worked yey');</script>";
     }
 ?>
