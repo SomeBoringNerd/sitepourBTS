54569629f9e7623a32dc5f5ead6bfe66ea3d1cf9
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-11-29 10:11:03 +0100

    Update entete.php

--- html/entete.php
+++ html/entete.php
@@ -35,7 +35,7 @@
 </head>
 
     <div id="topDuSite_mobile">
-        <img src="/rescources/tab.png" onclick="console.log('test')" name="bouton_toggle_mobile">
+        <img src="/rescources/tab.png" onclick="toggle()" name="bouton_toggle_mobile">
         <br>
         <div id="personnage" name="topmobile_child">
             <p>Menu principal</p>
@@ -49,10 +49,12 @@
         var togg1 = document.getElementByName("bouton_toggle_mobile");
         var d1 = document.getElementByName("topmobile_child");
 
-        togg1.addEventListener("click", () => {
+        d1.style.display = "none";
+
+        function toggle() {
             d1.style.display = (getComputedStyle(d1).display != "none") ? d1.style.display = "none" : d1.style.display = "block";
             console.log("d1 est actuellement a " + d1.style.display.toString());
-        })
+        }
     </script>
 
     <center>
