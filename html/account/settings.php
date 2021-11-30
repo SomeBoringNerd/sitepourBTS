ffe55b2133ae505b4eac9f4dcf220aa793566aa7
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-11-30 14:38:27 +0100

    Update settings.php

--- html/account/settings.php
+++ html/account/settings.php
@@ -7,3 +7,19 @@
     }    
     
 ?>
+
+
+
+<html>
+    <head>
+        <title>modifier mon profil</title>
+        <link rel="stylesheet" href="/index.css">
+    </head>
+
+    <body>
+        <?php include("/entete.php");?>
+        <br><br><br><br>
+
+
+    </body>
+</html>
