d73c1102ee6ae866893b792b1a261b22ffc3f7ee
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-11-30 14:43:02 +0100

    settings commit #2

--- html/account/settings.php
+++ html/account/settings.php
@@ -1,23 +1,28 @@
 <?php
+    // Initialize the session
     session_start();
-
-    if(!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] !== true){
-        header("location: login.php");
-        exit;
-    }    
     
+    // Check if the user is already logged in, if yes then redirect him to welcome page
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        header("location: ../index.php");
+        exit;
+
+    }
 ?>
-
-<html>
-    <head>
-        <title>modifier mon profil</title>
-        <link rel="stylesheet" href="../index.css">
-    </head>
-
+ 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="../index.css">
+</head>
     <body>
-        <?php include("/entete.php"); ?>
+        <?php include("../entete.php");?>
         <br><br><br><br>
-
-
+        <center>
+            <megaTitle>Connexion</megaTitle>
+            <p>Connectez vous ici.</p>
+        </center>
     </body>
 </html>
