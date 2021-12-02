c7ead97df987548193bc1257ec011c7e097563ab
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-02 14:35:34 +0100

    Update settings.php

--- html/account/settings.php
+++ html/account/settings.php
@@ -2,14 +2,7 @@
     session_start();
     require("../admin/config.php");
 
-    if(isset($_GET["id"]))
-    {
-        $USER_ID_TO_LOAD = $_GET["id"];
-    }
-    else
-    {
-        $USER_ID_TO_LOAD = $_SESSION["id"];
-    }
+    $USER_ID_TO_LOAD = $_SESSION["id"];
 
     if(isset($USER_ID_TO_LOAD)){
 
@@ -46,44 +39,10 @@
             }
         }
         else{
-            echo "<p>une erreur s'est produite : $link->error</p>";
+            header("location: login.php");
         }        
     }
 ?>
 
-                        <div id="POST_HISTORY">
-                            <center><p>Historique des postes</p>
-                            <p>______________________________</p></center>
-                            <?php
-                                require("../admin/config.php");
-                                if(isset($_GET["id"]))
-                                {
-                                    $USER_ID_TO_LOAD = $_GET["id"];
-                                }
-                                else
-                                {
-                                    $USER_ID_TO_LOAD = $_SESSION["id"];
-                                }
-                                $sql = "SELECT * FROM forum_post WHERE POST_AUTHOR_ID = $USER_ID_TO_LOAD";
-
-                                $result = $link->query($sql);
-
-                                if ($result->num_rows > 0) 
-                                {// logiquement, le résultat devrait retourner une seule valeur 
-                                // donc utiliser une boucle est valide même si c'est une mauvaise idée
-                                    while($row = $result->fetch_assoc()) 
-                                    {
-                                        $post_title = $row["POST_TITLE"];
-                                        $post_id = $row["POST_ID"];
-                                        echo "<a href=\"../forum/post.php?id=$post_id\"><p id=\"forum_account_p\">$post_title</p></a>";
-                                    }
-                                }else{
-                                    echo "<center><p>cet utilisateur n'a pas encore créé de postes.</p></center>";
-                                }
-
-                            ?>
-                            
-                        </div>
-
-                    </body>
-                </html>
+    </body>
+</html>
