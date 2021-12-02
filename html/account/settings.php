6addbc15d347cdc6ad6565599b71904807603a5a
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-02 13:58:24 +0100

    Update settings.php

--- html/account/settings.php
+++ html/account/settings.php
@@ -34,6 +34,8 @@
         $requete = mysql_query("SELECT * FROM users WHERE id = $USER_ID_TO_LOAD" OR die("la requete a échouée"));
         
         echo $requete;
+
+        echo "test";
         
         mysql_close(); 
         while($resultat = mysql_fetch_assoc($requete)) 
