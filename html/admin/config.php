c070b7d3651083aa72edfde4203d98968975fed0
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-08 09:32:34 +0100

    Update config.php

--- html/admin/config.php
+++ html/admin/config.php
@@ -3,13 +3,14 @@
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'admin');
     define('DB_PASSWORD', 'motdepasse');
-    define('DB_NAME', 'register_user_system');
+    define('DB_NAME', 'website_data_base');
 
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     
     // Check connection
     if($link === false){
-        echo "<script>console.log('fuck');</script>";
+        // très professionel de laisser ça en prod
+        // echo "<script>console.log('fuck');</script>";
         die("ERROR: Could not connect. " . mysqli_connect_error());
     }else{
         echo "<script>console.log('it worked yey');</script>";
