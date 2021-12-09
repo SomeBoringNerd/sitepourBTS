ce4200774a997e97baca9794b7266dffa148a4e1
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-09 14:34:22 +0100

    Update footer.php

--- html/footer.php
+++ html/footer.php
@@ -3,10 +3,10 @@
 ?>
 
 <footer>
-    <footer_text>Through The Dark by SomeBoringNerd</footer_text><footer_text id="right">_________</footer_text>
-    <footer_text>Copyright by YourAverageIndieGameCompany© 2021</footer_text><footer_text id="black">_________</footer_text>
-    <footer_text><a href="pages/mention.php">Mentions légales</a></footer_text>
-
+    <footer_text>Through The Dark by SomeBoringNerd</footer_text><footer_text id="right">_________</footer_text><br>
+    <footer_text>Copyright by YourAverageIndieGameCompany© 2021</footer_text><footer_text id="black">_________</footer_text><br>
+    <footer_text><a href="pages/mention.php">Mentions légales</a></footer_text><br>
+    <br>
     <br>
     <footer_text>in case of legal problem, please contact the owner of the website, 
         SomeBoringNerd, also known as Corentyn at this adress : 
