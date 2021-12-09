7f48afdd68ba9355681bcba708de2964e937c3a6
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-09 14:33:06 +0100

    Update footer.php

--- html/footer.php
+++ html/footer.php
@@ -2,22 +2,16 @@
     // mettre du code ici je suppose ?
 ?>
 
-<html>
-    <footer>
-        <table>
-            <tr>
-                <td><footer_text>Through The Dark by SomeBoringNerd</footer_text><footer_text id="black">_________</footer_text></td>
-                <td><footer_text id="black">_________</footer_text><footer_text>Copyright by YourAverageIndieGameCompany© 2021<br></footer_text></td>
-            </tr>
-            <tr>
-                <td><footer_text><a href="pages/mention.php">Mentions légales</a></footer_text></td>
-            </tr>
-        </table>
-    
-        <br>
-        <footer_text>in case of legal problem, please contact the owner of the website, 
-            SomeBoringNerd, also known as Corentyn at this adress : 
-            <a href="mailto:pro.corentynsauvage@gmail.com">pro.corentynsauvage@gmail.com</a><br>
-            abuse of the mail in any way, shape or form will result in me blocking you.</footer_text>
-    </footer>
-</html>
+<footer>
+    <table>
+
+        <td><footer_text>Through The Dark by SomeBoringNerd</footer_text><footer_text id="right">_________</footer_text>
+        <td><footer_text>Copyright by YourAverageIndieGameCompany© 2021</footer_text><footer_text id="black">_________</footer_text>
+        <td><footer_text><a href="pages/mention.php">Mentions légales</a></footer_text></td>
+
+    <br>
+    <footer_text>in case of legal problem, please contact the owner of the website, 
+        SomeBoringNerd, also known as Corentyn at this adress : 
+        <a href="mailto:pro.corentynsauvage@gmail.com">pro.corentynsauvage@gmail.com</a><br>
+        abuse of the mail in any way, shape or form will result in me blocking you.</footer_text>
+</footer>
