f4ce958b4829fc54ce9348d63a1e44ce886d8a14
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-09 14:13:25 +0100

    Update footer.php

--- html/footer.php
+++ html/footer.php
@@ -4,7 +4,11 @@
 
 <html>
     <footer>
-    Quelques informations de copyright ou bien quelques informations
-    sur l'auteur de l'article.
+    <p>Through The Dark by SomeBoringNerd</p><br>
+    <p>Copyright by SomeBoringCompany© 2021</p>
+    <p>in case of legal problem, please contact the owner of the website, 
+        SomeBoringNerd, also known as Corentyn at this adress : 
+        <a href="mailto:pro.corentynsauvage@gmail.com">pro.corentynsauvage@gmail.com</a><br>
+        abuse of the mail in any way, shape or form will result in me blocking you.
     </footer>
 </html>
