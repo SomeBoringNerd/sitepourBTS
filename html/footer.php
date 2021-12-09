f3a300db6119a18328dad01e354db064e157288f
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-09 14:41:01 +0100

    Réseaux Sociaux dans le footer

--- html/footer.php
+++ html/footer.php
@@ -3,9 +3,9 @@
 ?>
 
 <footer>
-    <footer_text>Through The Dark by SomeBoringNerd</footer_text><footer_text id="right">_________</footer_text><br>
-    <footer_text>Copyright by YourAverageIndieGameCompany© 2021</footer_text><footer_text id="black">_________</footer_text><br>
-    <footer_text><a href="pages/mention.php">Mentions légales</a></footer_text><br>
+    <footer_text>Through The Dark by SomeBoringNerd</footer_text><footer_text id="right"><a href="https://github.com/SomeBoringNerd">GitHub</a></footer_text><br>
+    <footer_text>Copyright by YourAverageIndieGameCompany© 2021</footer_text><footer_text id="right"><a href="https://www.youtube.com/channel/UCoQXdbuo7fGf12BTYCnJ1Eg">Youtube</a></footer_text><br>
+    <footer_text><a href="pages/mention.php">Mentions légales</a></footer_text><footer_text id="right"><a href="https://discord.gg/ZUA7pptwpe">Discord</a></footer_text><br>
     <br>
     <br>
     <footer_text>in case of legal problem, please contact the owner of the website, 
