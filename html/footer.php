36959c8e8c16f028cd1d5669024e728dc39288b6
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-09 14:08:53 +0100

    Footer

---
+++ html/footer.php
@@ -0,0 +1,10 @@
+<?php
+    // mettre du code ici je suppose ?
+?>
+
+<html>
+    <footer>
+    Quelques informations de copyright ou bien quelques informations
+    sur l'auteur de l'article.
+    </footer>
+</html>
