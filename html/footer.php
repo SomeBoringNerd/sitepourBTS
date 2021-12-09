00609af455f1d0f697bcb197c1de8c2deeffe6b4
SomeBoringNerd <pro.corentynsauvage@gmail.com>
2021-12-09 14:50:23 +0100

    Modification

--- html/footer.php
+++ html/footer.php
@@ -3,14 +3,20 @@
 ?>
 
 <footer>
-    <footer_text>Through The Dark by SomeBoringNerd</footer_text><footer_text id="right"><a href="https://github.com/SomeBoringNerd">GitHub</a></footer_text><br>
-    <footer_text>Copyright by YourAverageIndieGameCompany© 2021</footer_text><footer_text id="right"><a href="https://www.youtube.com/channel/UCoQXdbuo7fGf12BTYCnJ1Eg">Youtube</a></footer_text><br>
-    <footer_text><a href="pages/mention.php">Mentions légales</a></footer_text><footer_text id="right"><a href="https://discord.gg/ZUA7pptwpe">Discord</a></footer_text><br>
+    <footer_text>Through The Dark by SomeBoringNerd</footer_text><br>
+    <footer_text>Copyright by YourAverageIndieGameCompany© 2021</footer_text><br>
+    <footer_text><a href="pages/mention.php">Mentions légales</a></footer_text>
     <br>
+
     <br>
-    <footer_text>in case of legal problem, please contact the owner of the website, 
+    <footer_text>in case of legal problem, or if you want to hire please contact the owner of the website, 
         SomeBoringNerd, also known as Corentyn at this adress : 
         <a href="mailto:pro.corentynsauvage@gmail.com">pro.corentynsauvage@gmail.com</a><br>
-        If you represent a legal entity, please use a real email (like @gov.fr or @cnil.fr) and not a personal one.<br>
+        Please use a professional email if you need to contact me for legal reason or for hiring me<br>
     </footer_text>
+    <p> Social Networks :</p>
+    <footer_text id="right"><a href="https://github.com/SomeBoringNerd">GitHub</a></footer_text><br>
+    <footer_text id="right"><a href="https://www.youtube.com/channel/UCoQXdbuo7fGf12BTYCnJ1Eg">Youtube</a></footer_text><br>
+    <footer_text id="right"><a href="https://discord.gg/ZUA7pptwpe">Discord</a></footer_text><br>
+    <pb>Looking for a dev ? feel free to contact me on my professional email</pb>
 </footer>
