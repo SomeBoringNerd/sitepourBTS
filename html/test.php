229b42f1f16b6457ddf89addc556ee113112dbe7
A Boring Nerd <pro.corentynsauvage@gmail.com>
2021-12-14 10:08:58 +0100

    Update test.php

--- html/test.php
+++ html/test.php
@@ -1,21 +1,10 @@
 <?php
-function file_get_contents_curl($url) {
-    $ch = curl_init();
-  
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_URL, $url);
-  
-    $data = curl_exec($ch);
-    curl_close($ch);
-  
-    return $data;
-}
-  
-$data = file_get_contents_curl(
-'https://media.geeksforgeeks.org/wp-content/uploads/geeksforgeeks-6-1.png');
-  
-$fp = 'logo-1.png';
-  
-file_put_contents( $fp, $data );
+ 
+ $url = 
+ 'https://media.geeksforgeeks.org/wp-content/uploads/geeksforgeeks-6-1.png'; 
+   
+ $img = 'logo.png'; 
+   
+ // Function to write image into file
+ file_put_contents($img, file_get_contents($url));
 ?>
